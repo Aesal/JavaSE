6423f4e716482dfe5b0a95def80936145f5ff08f
nanwanyou <nww0419@gmail.com>
2023-08-12 09:29:20 +0900

    커밋 테스트

--- src/section02/Variable07.java
+++ src/section02/Variable07.java
@@ -3,13 +3,13 @@ package section02;
 public class Variable07 {
 
     public static void main(String[] args) {
-//        byte iByte = 10;
-//        int iInt = iByte;
-//        System.out.println("iInt: " + iInt);
-//
-//        float iFloat = 10.1f;
-//        double idouble = iFloat;
-//
+        byte iByte = 10;
+        int iInt = iByte;
+        System.out.println("iInt: " + iInt);
+
+        float iFloat = 10.1f;
+        double idouble = iFloat;
+
 
     }
 }
