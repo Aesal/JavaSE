bd912ac1e60199921fc081c63d99387a65e8c22a
nanwanyou <nww0419@gmail.com>
2023-08-12 09:23:46 +0900

    수업 종료

---
+++ src/section02/Variable07.java
@@ -0,0 +1,10 @@
+package section02;
+
+public class Variable07 {
+
+    public static void main(String[] args) {
+        byte iByte = 10;
+        int iInt = iByte;
+        System.out.println("iInt: " + iInt);
+    }
+}
