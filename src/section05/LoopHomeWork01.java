c9b3accf62019e0e933f36335ac8e6ee62ffed3e
Aesal <99933395+Aesal@users.noreply.github.com>
2023-08-14 16:06:56 +0900

    Update LoopHomeWork01.java
    for문 1개씩 제거

--- src/section05/LoopHomeWork01.java
+++ src/section05/LoopHomeWork01.java
@@ -79,11 +79,7 @@ public class LoopHomeWork01 {
 				System.out.print("0");
 			}
 
-			for (int j=0; j<=i; j++) {
-				System.out.print("*");
-			}
-
-			for (int j=0; j<i; j++) {
+			for (int j=0; j<=i*2; j++) {
 				System.out.print("*");
 			}
 
@@ -103,16 +99,11 @@ public class LoopHomeWork01 {
 		//4번
 		for (int i = 0; i <= maxCount; i++) {
 
-
 			for (int j = 0; j<i; j++) {
 				System.out.print("0");
 			}
 
-			for (int k = maxCount; k>=i; k--) {
-				System.out.print("*");
-			}
-
-			for (int k = maxCount; k > i; k--) {
+			for (int k = maxCount*2; k>=i*2; k--) {
 				System.out.print("*");
 			}
 
