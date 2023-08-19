4213db4a7d2a008acb57aed31c014fd16992d05c
nanwanyou <nww0419@gmail.com>
2023-08-19 09:22:09 +0900

    수업종료

--- src/section05/LoopHomeWork01.java
+++ src/section05/LoopHomeWork01.java
@@ -14,11 +14,19 @@ public class LoopHomeWork01 {
 
     public static void main(String[] args) {
 
-        int maxCount = 4;
+        int maxcount = 3;
 
-        Work01(maxCount);
-        Work02(maxCount);
-        Work03(maxCount);
+        Work01(maxcount);
+        System.out.println("-----------------------------------------");
+
+        Work02(maxcount);
+        System.out.println("-----------------------------------------");
+
+        Work03(maxcount);
+        System.out.println("-----------------------------------------");
+
+        Work04(maxcount);
+        System.out.println("-----------------------------------------");
 
 
     }
@@ -29,30 +37,28 @@ public class LoopHomeWork01 {
 
 
 
-  public static void Work01(int maxCount){
-      //1번
-      for (int i = 0; i <maxCount; i++) {
+    public static void Work01(int maxCount){
+        //1번
+        for (int i = 0; i <maxCount; i++) {
 
-          for (int j=0; j<=i; j++) {
-              System.out.print("*");
-          }
+            for (int j=0; j<=i; j++) {
+                System.out.print("*");
+            }
 
-          for (int k=maxCount; k>i; k--){
-              System.out.print(" ");
-          }
+            for (int k=maxCount; k>i; k--){
+                System.out.print("0");
+            }
 
-          System.out.println("");
-      } // end of for
-
-      System.out.println("-----------------------------------------");
-  }
+            System.out.println("");
+        } // end of for
+    }
 
     public static void Work02(int maxCount) {
         //2번
         for (int i = 0; i < maxCount; i++) {
 
             for (int j = maxCount; j>i; j--) {
-                System.out.print(" ");
+                System.out.print("0");
             }
 
             for (int k=0; k<=i; k++) {
@@ -62,8 +68,6 @@ public class LoopHomeWork01 {
             System.out.println("");
 
         } // end of for
-
-        System.out.println("-----------------------------------------");
     }
 
     public static void Work03(int maxCount) {
@@ -72,25 +76,44 @@ public class LoopHomeWork01 {
         for (int i = 0; i < maxCount; i++) {
 
             for (int j = maxCount; j>i; j--) {
-                System.out.print(" ");
+                System.out.print("0");
             }
 
-            for (int j=0; j<=i; j++) {
+            for (int j=0; j<=i*2; j++) {
                 System.out.print("*");
             }
 
-            for (int j=0; j<i; j++) {
-                System.out.print("*");
+            for (int j = maxCount; j>i; j--) {
+                System.out.print("0");
             }
-            System.out.println(" ");
+
+            System.out.println("");
 
         } // end of for
-        System.out.println("-----------------------------------------");
     }
 
     public static void Work04(int maxCount) {
-        System.out.println("-----------------------------------------");
+        //같은문장 반복이라 호출.
+        Work03(maxCount);
 
+        //4번
+        for (int i = 0; i <= maxCount; i++) {
+
+            for (int j = 0; j<i; j++) {
+                System.out.print("0");
+            }
+
+            for (int k = maxCount*2; k>=i*2; k--) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j<i; j++) {
+                System.out.print("0");
+            }
+
+            System.out.println("");
+
+        }//end of for
     }
 
 
--- src/section05/LoopHomeWork02.java
+++ src/section05/LoopHomeWork02.java
@@ -1,4 +1,21 @@
 package section05;
 
 public class LoopHomeWork02 {
+
+     public static void main(String[] args) {
+        int[][] iArray = {{2,4},{5,7},{8,9}};
+        for (int[] num : iArray){
+            for (int i = 1; i <= 9; i++) {
+                for (int j = num[0]; j <= num[1]; j++) {
+                    System.out.print(multiply(j,i));
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+    public static String multiply(int i, int j) {
+        return String.format("%2d  X %2d = %-3d     ", i, j, i * j);
+    }
+
 }
